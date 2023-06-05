7f7f716bcb9d484f53b200726ba732730e316241
VarshavetsAleksandr <varshavetsaleksandr@gmail.com>
2023-06-06 02:00:30 +0300

    updata

--- js/modal-gpt-new.js
+++ js/modal-gpt-new.js
@@ -2,10 +2,12 @@
     function openModal(modalNumber) {
       var modal = document.getElementById("modal" + modalNumber);
       modal.style.display = "block";
+      modal.classList.remove("is-hidden");
     }
 
     // Функция для закрытия модального окна
     function closeModal(modalNumber) {
       var modal = document.getElementById("modal" + modalNumber);
       modal.style.display = "none";
+      modal.classList.remove("is-hidden");
     }
